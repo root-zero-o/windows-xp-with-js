2b56699414cae794d89591a788587bbd11ed2b84
root-zero-o <rootzero17@gmail.com>
2024-11-27 14:30:50 +0900

    이벤트 등록 - 메소드화

--- src/core/Component.js
+++ src/core/Component.js
@@ -5,6 +5,7 @@ export default class Component {
     this.$target = $target;
     this.setup();
     this.render();
+    this.setEvent();
   }
   setup() {}
   template() {
@@ -12,11 +13,18 @@ export default class Component {
   }
   render() {
     this.$target.innerHTML = this.template();
-    this.setEvent();
   }
   setEvent() {}
   setState(newState) {
     this.state = { ...this.state, ...newState };
     this.render();
+  }
+
+  addEvent(eventType, selector, callback) {
+    const children = [...this.$target.querySelectorAll(selector)];
+    this.$target.addEventListener(eventType, (event) => {
+      if (!event.target.closest(selector)) return false;
+      callback(event);
+    });
   }
 }
